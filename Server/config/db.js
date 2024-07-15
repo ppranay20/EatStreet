8a931ad124e24aab8a4a59be5cf25ecb88d8776c
ppranay20 <pranay20.parakh@gmail.com>
2024-07-15 21:31:50 +0530

    Ready to deploy

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
-    mongoose.connect("mongodb+srv://20pranayparakh:XAUWbIARWrFFsWFm@cluster0.6ufulsd.mongodb.net/")
+    mongoose.connect(process.env.MONGODB_URL)
         .then((res) => console.log("Database Connected Successfully"))
         .catch((err) => console.log("Error in connecting to databse -> " + err))
 }
